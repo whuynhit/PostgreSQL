d23b721b1670fff8d0d7992399e37ecb01be1566
whuynhit <142070438+whuynhit@users.noreply.github.com>
2026-07-21 10:10:04 -0700

    Update View Long-Running Transaction.sql

--- View Long-Running Transaction.sql
+++ View Long-Running Transaction.sql
@@ -1,5 +1,5 @@
 -- View Long-Running Transaction
-SELECT pid, usename, application_name, client_addr, state, query,
+SELECT pid, usename, datname, application_name, client_addr, state, query,
        now() - xact_start AS xact_age, now() - query_start AS query_age
 FROM pg_stat_activity
 WHERE xact_start IS NOT NULL

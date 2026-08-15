b4ced64ddb574e14dff3fabf8b152ea396520781
whuynhit <142070438+whuynhit@users.noreply.github.com>
2026-08-14 17:25:04 -0700

    Update pg_stat_statements.sql

--- pg_stat_statements.sql
+++ pg_stat_statements.sql
@@ -1,3 +1,15 @@
+-- Old query
 SELECT query, calls, total_exec_time, rows, mean_exec_time
 FROM pg_stat_statements
 WHERE queryid = '<queryid_from_PI>';
+
+-- Query for written temp file size
+SELECT
+    queryid,
+    calls,
+    temp_blks_written,
+    pg_size_pretty(temp_blks_written * 8192) AS temp_written,
+    query
+FROM pg_stat_statements
+ORDER BY temp_blks_written DESC
+LIMIT 20;

5db7f968346b3e4d6d4df2afbd85d57a0b0adff7
whuynhit <142070438+whuynhit@users.noreply.github.com>
2026-08-23 02:18:30 -0700

    Update Query Plan Node Cost Extraction.sql

--- Query Plan Node Cost Extraction.sql
+++ Query Plan Node Cost Extraction.sql
@@ -1,21 +1,21 @@
---Query Plan Node Cost Extraction
+-- Generate cmdlet script to extract Query Plan Node Cost
 -- Provide the following connection details and query
 -- Then run the output in Windows CMD
-SET psql.dir = 'C:\Program Files\PostgreSQL\15\bin\psql.exe';
-SET psql.host = '[host]';
-SET psql.port = '[port]';
-SET psql.username = '[user]';
-SET psql.database = '[database]';
-SET psql.space = ' ';
-SET psql.char = '^';
-SET psql.query = $SQL$<multi-line query here>$SQL$;
+SET util.dir = 'C:\Program Files\PostgreSQL\15\bin\psql.exe';
+SET util.host = '[host]';
+SET util.port = '[port]';
+SET util.username = '[user]';
+SET util.database = '[database]';
+SET util.space = ' ';
+SET util.char = '^';
+SET util.query = $SQL$<multi-line query here>$SQL$;
 
 SELECT 
-	'"' || current_setting('psql.dir') || '"' ||current_setting('psql.space') || current_setting('psql.char')
-	|| chr(10) || '-h '||current_setting('psql.host') || current_setting('psql.space') || current_setting('psql.char')
-	|| chr(10) || '-p '||current_setting('psql.port') || current_setting('psql.space') || current_setting('psql.char')
-	|| chr(10) || '-U '||current_setting('psql.username') || current_setting('psql.space') || current_setting('psql.char')
-	|| chr(10) || '-d '||current_setting('psql.database') || current_setting('psql.space') || current_setting('psql.char')
-	|| chr(10) || '--pset=pager=off' || current_setting('psql.space') || current_setting('psql.char') -- Display all results instead of paginated results
-	|| chr(10) || '-c "' || regexp_replace(current_setting('psql.query'), E'\n', ' ', 'g') || '"' || ' | findstr "cost="' -- Collapse multi-line query into 1 line
+	'"' || current_setting('util.dir') || '"' ||current_setting('util.space') || current_setting('util.char')
+	|| chr(10) || '-h '||current_setting('util.host') || current_setting('util.space') || current_setting('util.char')
+	|| chr(10) || '-p '||current_setting('util.port') || current_setting('util.space') || current_setting('util.char')
+	|| chr(10) || '-U '||current_setting('util.username') || current_setting('util.space') || current_setting('util.char')
+	|| chr(10) || '-d '||current_setting('util.database') || current_setting('util.space') || current_setting('util.char')
+	|| chr(10) || '--pset=pager=off' || current_setting('util.space') || current_setting('util.char') -- Display all results instead of paginated results
+	|| chr(10) || '-c "' || regexp_replace(current_setting('util.query'), E'\n', ' ', 'g') || '"' || ' | findstr "cost="' -- Collapse multi-line query into 1 line
 ;

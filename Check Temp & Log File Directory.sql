4fd75605ad71432d06448867543d24ed7fe42281
whuynhit <142070438+whuynhit@users.noreply.github.com>
2026-08-26 15:22:31 -0700

    Update Check Temp & Log File Directory.sql

--- Check Temp & Log File Directory.sql
+++ Check Temp & Log File Directory.sql
@@ -1,3 +1,40 @@
+-- Check temp file count, file size and other relevant query information by query PID
+SELECT 
+    t.pid,  
+    t.count, 
+    t.sum_size, 
+    t.pretty_size,
+	a.usename,
+	a.datname,
+	a.application_name,
+	a.backend_type,
+	a.client_addr,
+	a.wait_event_type,
+	a.wait_event,
+    a.state,
+	a.query,
+	now() - a.xact_start AS xact_age,
+	now() - a.query_start AS query_age
+FROM (
+	SELECT
+		REPLACE(LEFT(name, strpos(name, '.') - 1), 'pgsql_tmp', '')::integer as pid, 
+		COUNT(*) AS count, 
+		SUM(size) AS sum_size,
+		pg_size_pretty(SUM(size)) AS pretty_size
+	FROM pg_ls_tmpdir() 
+	GROUP BY pid
+) t
+JOIN pg_stat_activity a ON t.pid = a.pid;
+
+-- Check temp file count & size by PID
+SELECT
+	REPLACE(LEFT(name, strpos(name, '.')-1),'pgsql_tmp','') as pid, 
+	COUNT(*), 
+	SUM(size),
+	pg_size_pretty(SUM(size))
+FROM pg_ls_tmpdir() 
+GROUP BY pid;
+
 -- Check temp File directory for temp files, and their size (Consumes FreeLocalStorage)
 SELECT 
 	name,

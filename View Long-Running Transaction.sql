bc70a3b7eed62ea2e9c937274ac4f60234126bdf
whuynhit <142070438+whuynhit@users.noreply.github.com>
2026-09-01 02:33:54 -0700

    Update View Long-Running Transaction.sql

--- View Long-Running Transaction.sql
+++ View Long-Running Transaction.sql
@@ -1,6 +1,7 @@
 -- View Long-Running Transaction
 SELECT --'SELECT pg_terminate_backend(' || pid || ');',
-	pid, usename, datname, application_name, backend_type, client_addr, wait_event_type, wait_event, state, query,
+	pid, usename, datname, application_name, backend_type, client_addr, 
+	wait_event_type || ':' || wait_event, state, query,
 	now() - xact_start AS xact_age, now() - query_start AS query_age
 FROM pg_stat_activity
 WHERE xact_start IS NOT NULL --AND backend_type != 'parallel worker' 

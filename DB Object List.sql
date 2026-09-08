9778037c0126726834e3d3b45407801134c14d78
whuynhit <142070438+whuynhit@users.noreply.github.com>
2026-09-08 04:51:40 -0700

    Update DB Object List.sql

--- DB Object List.sql
+++ DB Object List.sql
@@ -22,13 +22,12 @@ JOIN pg_namespace n ON n.oid = c.relnamespace
 WHERE n.nspname NOT IN ('information_schema', 'pg_catalog', 'pg_toast')
 -- AND c.relispartition = false -- Uncomment to filter out Child Partitions
 ORDER BY schema_name, object_name;
--- ORDER BY pg_total_relation_size(c.oid) DESC;
+-- ORDER BY pg_total_relation_size(c.oid) DESC; -- Sort by largest object size
 
 -- List object count by schema and object type
 SELECT
-    current_database() AS database,
-    DISTINCT n.nspname AS schema_name,
-  	c.relkind,
+    DISTINCT current_database() AS database,
+    n.nspname AS schema_name,
     CASE 
   	WHEN c.relkind = 'r' THEN 'table'
   	WHEN c.relkind = 'i' THEN 'index'
@@ -41,11 +40,29 @@ SELECT
   	WHEN c.relkind = 'p' THEN 'partitioned table'
   	WHEN c.relkind = 'I' THEN 'partitioned index'
   	ELSE 'other'
-    END AS type,
+    END AS object_type,
     COUNT (1)
 FROM pg_class c
 JOIN pg_namespace n ON n.oid = c.relnamespace
 WHERE n.nspname NOT IN ('information_schema', 'pg_catalog', 'pg_toast')
 -- AND c.relispartition = false -- Uncomment to filter out Child Partitions
-GROUP BY schema_name, c.relkind,
-ORDER BY schema_name, type;
+GROUP BY schema_name, c.relkind
+ORDER BY schema_name, object_type;
+
+-- Lists tables and table statistics [Partitioned Tables (p) and Tables (r) only, no Materialized Views (m)]
+SELECT s.*
+FROM pg_stat_user_tables s
+JOIN pg_class c 
+ON s.relid = c.oid
+WHERE c.relkind IN ('p', 'r') -- Comment out to show all tables + materialized views 
+AND c.relispartition = false -- Filters out Child Partitions
+ORDER BY schemaname, relname;
+
+-- List tables and table owner
+SELECT t.*
+FROM pg_tables t
+JOIN pg_namespace n ON t.schemaname = n.nspname
+JOIN pg_class c ON t.tablename = c.relname AND c.relnamespace = n.oid
+WHERE t.schemaname NOT IN ('information_schema', 'pg_catalog', 'pg_toast')
+AND NOT c.relispartition -- Filters out Child Partitions
+ORDER BY t.schemaname, t.tablename;

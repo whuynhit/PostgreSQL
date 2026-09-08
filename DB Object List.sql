ecc7484542d33f151a7eef5d45968b971ffd6628
whuynhit <142070438+whuynhit@users.noreply.github.com>
2026-09-08 07:39:59 -0700

    Update DB Object List.sql

--- DB Object List.sql
+++ DB Object List.sql
@@ -24,6 +24,30 @@ WHERE n.nspname NOT IN ('information_schema', 'pg_catalog', 'pg_toast')
 ORDER BY schema_name, object_name;
 -- ORDER BY pg_total_relation_size(c.oid) DESC; -- Sort by largest object size
 
+-- List object count by object type
+SELECT
+    DISTINCT current_database() AS database,
+    CASE 
+  	WHEN c.relkind = 'r' THEN 'table'
+  	WHEN c.relkind = 'i' THEN 'index'
+  	WHEN c.relkind = 'S' THEN 'sequence'
+  	WHEN c.relkind = 't' THEN 'TOAST'
+  	WHEN c.relkind = 'v' THEN 'view'
+  	WHEN c.relkind = 'm' THEN 'materialized view'
+  	WHEN c.relkind = 'c' THEN 'composite type'
+  	WHEN c.relkind = 'f' THEN 'foreign table'
+  	WHEN c.relkind = 'p' THEN 'partitioned table'
+  	WHEN c.relkind = 'I' THEN 'partitioned index'
+  	ELSE 'other'
+    END AS object_type,
+    COUNT (1)
+FROM pg_class c
+JOIN pg_namespace n ON n.oid = c.relnamespace
+WHERE n.nspname NOT IN ('information_schema', 'pg_catalog', 'pg_toast')
+-- AND NOT c.relispartition -- Uncomment to filter out Child Partitions
+GROUP BY c.relkind
+ORDER BY object_type;
+
 -- List object count by schema and object type
 SELECT
     DISTINCT current_database() AS database,
